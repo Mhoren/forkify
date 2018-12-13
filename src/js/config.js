b1c622a039836d5fc20fd08e69f520eca3ddce00
Mhoren <41128778+Mhoren@users.noreply.github.com>
2018-12-13 22:46:32 +0100

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/'
+export const key = `74c81559e0e2c4ca3e0abfbad0812fea`
+
+// https://www.food2fork.com/api/search
+// 74c81559e0e2c4ca3e0abfbad0812fea
+// 0048fb69034977ec93eaba68e3b32134
